18712b6f63f14e58796f04af3c5b56207ae742eb
chunws <dntkd51@naver.com>
2024-05-15 21:32:58 +0900

    chart view edit

--- back_testing.py
+++ back_testing.py
@@ -29,43 +29,44 @@ class Back_Testing:
 
         self.coin_history = {}
         self.error = 0
-        self.start_date = datetime.datetime.now()
+        self.start_date = datetime.datetime(2024, 5, 14)
 
         for day in range(duration, 0, -1):
-            self.duration.append(self.start_date - datetime.timedelta(days=day) - datetime.timedelta(days=30))
+            self.duration.append(self.start_date - datetime.timedelta(days=day))
+    
+    def view_hour_chart(self, ticker, low, high, day):
+        invest_status = False
         
+        low_index, high_index = math.inf, 0
+
+        chart = pyupbit.get_ohlcv_from(ticker = ticker,
+                                       fromDatetime = day, to = day + datetime.timedelta(days=1), 
+                                       interval="minute60")
+        
+        for index in range(9, len(chart)):
+            if chart["high"].iloc[index] >= high:
+                high_index = max(high_index, index)
+            
+            if chart["low"].iloc[index] <= low:
+                invest_status = True
+                low_index = min(low_index, index)
+
+        sequence = high_index > low_index
+        return [invest_status, sequence]
+    
     def simulate(self):
         for day in self.duration:
-            message = f"----- {day.year}-{day.month}-{day.day} 투자 시뮬레이션 -----"
-            testing_bot.send_message(message)
-
             coin_list = check_bull_market(day, 5) # 상승장 코인 검색
             check_win = self.end_seed
 
             if len(coin_list) == 0:
-                testing_bot.send_message("오늘은 투자 할 대상이 없습니다.")
+                print("오늘은 투자 할 대상이 없습니다.")
             
             for coin in coin_list:
                 coin_seed = int(self.end_seed / len(coin_list))
                 after_coin_seed = 0
 
-                ### 변동성 돌파 전략 도입 시 예상 마감 가격 산정 ###
-                 
-                # target_buy = target_buy_amount(coin = coin, target_date = day)
-
-                # if math.isnan(target_buy):
-                #     self.error += 1
-                #     testing_bot.send_message("목표 매수가 불러오기 에러")
-                #     continue
-
-                # if target_buy >= coin_list[coin]:
-                #     continue
-                
-                ###
                 pre_high, pre_low = coin_list[coin]["high"], coin_list[coin]["low"]
-                
-                message = f"> {coin} 예상 고가: {round(pre_high,2):2,} 예상 저가: {round(pre_low, 2):2,}"
-                testing_bot.send_message(message)
 
                 self.end_seed -= coin_seed
 
@@ -75,37 +76,20 @@ class Back_Testing:
                 time.sleep(0.5)
                 chart = pyupbit.get_ohlcv_from(ticker = coin, fromDatetime = day - datetime.timedelta(days=1), to = day)
                 
-                invest_status = False
-                
-                if chart is None:
-                    self.error += 1
-                    self.end_seed += coin_seed
-                    
-                    testing_bot.send_message(str(coin_list) + " 중 " + coin + "차트 불러오기 에러")
-                    continue
+                invest_status, sequence = self.view_hour_chart(coin, pre_low, pre_high, day)
                 
                 high_price = chart["high"].iloc[-1]
                 low_price = chart["low"].iloc[-1]
                 close_price = chart["close"].iloc[-1]
-                
-                message = f"고가: {round(high_price,2):2,} \n저가: {round(low_price,2):2,} \n종가: {round(close_price,2):2,}"
-                testing_bot.send_message(message)
 
-                if low_price <= pre_low:
-                    invest_status = True
-                    
-                    if high_price >= pre_high:
+                if invest_status:
+                    if sequence:
                         variance =  (pre_high - pre_low) / pre_low
                         
                     else:
                         variance =  (close_price - pre_low) / pre_low
                     
                     after_coin_seed = coin_seed + coin_seed * variance * 0.9995
-
-                    testing_bot.send_message(f"{coin} 결과: {int(after_coin_seed - coin_seed):2,}")
-                    
-                else:
-                    testing_bot.send_message(f"{coin} 결과: 저가 미 도달")
                     
                 self.end_seed += after_coin_seed if invest_status else coin_seed
                 self.coin_history[coin]  += after_coin_seed - coin_seed if invest_status else after_coin_seed
@@ -114,31 +98,23 @@ class Back_Testing:
             self.max_revenu_ratio = max(self.max_revenu_ratio, total_variance)
             self.max_loss_ratio = min(self.max_loss_ratio, total_variance)
 
-            if check_win > self.end_seed:
+            if check_win < self.end_seed:
                 self.revenu_count += 1
             
-            elif check_win < self.end_seed :
+            elif check_win > self.end_seed :
                 self.loss_count += 1
 
-            message = f"투자 결과: {int(check_win):2,} => {int(self.end_seed):2,} ({ round((self.end_seed - check_win) / check_win * 100, 2)}%)\n"
-            testing_bot.send_message(message)
+            message = f"{day} 투자 결과: {int(check_win):2,} => {int(self.end_seed):2,} ({ round((self.end_seed - check_win) / check_win * 100, 2)}%)\n"
+            print(message)
             
-        testing_bot.send_message("----- 백테스팅 결과 -----")
-        testing_bot.send_message(f"테스트 기간: {self.duration[0]} ~ {self.duration[-1]}")
-        testing_bot.send_message("코인 별 수익 내역")
-        
-        for history in self.coin_history:
-            time.sleep(0.5)
-            if self.coin_history[history] != 0:
-                testing_bot.send_message(f"{history} : {int(self.coin_history[history]):2,}")
-        
-        testing_bot.send_message(f"테스트 기간: {len(self.duration)}일 \n수익 기간: {self.revenu_count}일 \n손실 기간: {self.loss_count}일")
-        testing_bot.send_message(f"\n최대 수익률: {self.max_revenu_ratio:2,}% \n최저 수익률: {self.max_loss_ratio:2,}% \n")
-        testing_bot.send_message(f"시작 금액 : {self.start_seed:2,} \n마감 금액 : {int(self.end_seed):2,}")
-        testing_bot.send_message(f"수익률: {round((self.end_seed - self.start_seed) / self.start_seed * 100, 2):2,}%")
+        print("----- 백테스팅 결과 -----")
+        print(f"테스트 기간: {len(self.duration)}일 \n수익 기간: {self.revenu_count}일 \n손실 기간: {self.loss_count}일")
+        print(f"\n최대 수익률: {self.max_revenu_ratio:2,}% \n최저 수익률: {self.max_loss_ratio:2,}% \n")
+        print(f"시작 금액 : {self.start_seed:2,} \n마감 금액 : {int(self.end_seed):2,}")
+        print(f"수익률: {round((self.end_seed - self.start_seed) / self.start_seed * 100, 2):2,}%")
 
 if __name__ == "__main__":
-    setting = Back_Testing(100000, 180)
+    setting = Back_Testing(399574, 180)
     setting.simulate()
     
     
