074fa75977200407de6d0c73201ca7218fe3d2d4
Chunws13 <dntkd51@naver.com>
2024-08-06 22:42:47 +0900

    분 단위 캔들 호출 로직 추가

--- async_request.py
+++ async_request.py
@@ -19,8 +19,13 @@ def data_format(response_data):
     return df
 
 
-async def fetch_price(session, ticker, date):
+async def fetch_price(session, ticker, date, minutes=None):
     url = f"https://api.upbit.com/v1/candles/days"
+    
+    if minutes is not None:
+        date -= datetime.timedelta(hours=9)
+        url = f"https://api.upbit.com/v1/candles/minutes/{minutes}"
+            
     params = {
         'market': ticker,
         'to': date.strftime("%Y-%m-%dT%H:%M:%S"),
@@ -32,20 +37,19 @@ async def fetch_price(session, ticker, date):
 
         if response.status == 429:
             await asyncio.sleep(1)
-            return await fetch_price(session, ticker, date)
+            return await fetch_price(session, ticker, date, minutes)
         
-        # print(data)
         data_frame = data_format(data)
         return [ticker, data_frame]
     
 
-async def fetch_prices(tickers, end_date):
+async def fetch_prices(tickers, end_date, minutes=None):
     async with aiohttp.ClientSession() as session:
         tasks = []
         dictionary_info = {}
 
         for ticker in tickers:
-            tasks.append(fetch_price(session, ticker, end_date))
+            tasks.append(fetch_price(session, ticker, end_date, minutes))
 
         prices = await asyncio.gather(*tasks)
         for price in prices:
@@ -53,15 +57,14 @@ async def fetch_prices(tickers, end_date):
 
         return dictionary_info
 
-def get_ticekr_info(target_date, ticker_list):
-    tickers_info = asyncio.run(fetch_prices(ticker_list, target_date))
+def get_ticekr_info(target_date, ticker_list, minutes=None):
+    tickers_info = asyncio.run(fetch_prices(ticker_list, target_date, minutes))
     return tickers_info
 
 if __name__ == "__main__":
-    start = datetime.datetime.now()
-    tickers_info = get_ticekr_info(datetime.datetime(2024, 6, 19))
+    # 분 단위 요청을 하면 UTC 기준으로 데이터가 반환된다 
+    # 함수에 분 단위로 요청이 들어오면 9시간을 빼서 구현
+    tickers_info = get_ticekr_info(datetime.datetime(2024, 8, 6), ["KRW-BTC"], 10)
     for ticker in tickers_info:
-        print(ticker, tickers_info[ticker].iloc[-1])
-    end = datetime.datetime.now()
-
-    print(f"running time: {end - start}")
+        print(ticker, tickers_info[ticker])
+    
