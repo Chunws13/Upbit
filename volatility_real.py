8b7ab26badcd2652168203f62251fecf9fe66f15
Chunws <32253467+Chunws13@users.noreply.github.com>
2025-06-16 14:54:08 +0900

    투자금액 산정 방법 변경

--- volatility_real.py
+++ volatility_real.py
@@ -40,7 +40,7 @@ class Upbit_User:
         self.budget = self.user.get_balance("KRW")
 
         self.investment_size = 4
-        self.investment_amount = (self.budget // self.investment_size) // 10000 * 10000
+        self.investment_amount = ((self.budget // self.investment_size) // 10) * 10
 
         messanger.send_message(f"{self.today.year}년 {self.today.month}월 {self.today.day}일 자동 투자 매매 봇이 연결되었습니다.")
         
@@ -57,7 +57,7 @@ class Upbit_User:
 
     def adjust_budget(self):
         self.budget = self.user.get_balance("KRW")
-        self.investment_amount = (self.budget // self.investment_size) // 100 * 100
+        self.investment_amount = ((self.budget // self.investment_size) // 10) * 10
         messanger.send_message(f"잔고 : {self.budget}")
         
         if self.budget <= 10000:
